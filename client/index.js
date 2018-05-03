fd0db1f52ac4eb3e1d71c3520b2a4a0d5b1c96ad
Rupam <rupam.shaw@gmail.com>
2018-05-04 02:30:56 +1000

    backend for signin logout signup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 const Root = () => {
   return (
+
     <div>
       Auth Starter
     </div>
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,18 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require('./user_type')
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields:{
+    currentUser: { 
+      type: UserType ,
+      resolve(parentValue,args,req){
+        console.log("passport returned user i.e currentuser",req.user)
+        return req.user //null if user not signin returned by passport
+      }
+    }
+  }
 });
 
 module.exports = RootQueryType;
